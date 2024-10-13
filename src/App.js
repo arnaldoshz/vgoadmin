041aa6384d31322f7980a34def185ebb41678df6
AjSHZ <mesgps@live.com>
2024-10-13 13:10:31 -0400

    miel

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Hola Miel y Oro</code> and save to reload.
+          Hola <code>Hola Miel y Oro</code> Para Ti Siempre.
         </p>
         <a
           className="App-link"
