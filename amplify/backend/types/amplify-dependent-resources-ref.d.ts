3bfd739ec9ed48c129f8c02c8d996ccb01ab2cda
AjSHZ <mesgps@live.com>
2024-10-13 17:42:57 -0400

    Added graphql and storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s3335a6c25": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
